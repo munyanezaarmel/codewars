2634b6f6541b95357a2c1e94ddeb69beb9a7057a
unknown <munyaarmel61@gmail.com>
2023-04-02 16:19:47 +0200

    IIFE

--- bind.js
+++ bind.js
@@ -36,9 +36,31 @@ Test data for bonus:
 Hints: Use many of the tools you learned about in this and the last section �
 */
 const poll = {
- question: "What is your favourite programming language?",
- options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
- // This generates [0, 0, 0, 0]. More in the next section!
- answers: new Array(4).fill(0),
-}
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const number = Number(
+      prompt(
+        "What is your favourite programming language? 0: JavaScript ,1: Python,2: Rust,3: C++"
+      )
+    );
+    typeof number === "number" &&
+      number < this.options.length &&
+      this.answers[number]++;
+      console.log(this.answers);
+      this.displayResults('string')
+  },
+
+    displayResults(type='array') {
+        if (type === 'array') {
+          console.log(this.answers)
+        }
+        else if (type === 'string') {
+            console.log(`the poll string is${this.answers.join(', ')}`)
+        }
+  }
+};
+document.querySelector('.button').addEventListener('click',poll.registerNewAnswer.bind(poll))
  
