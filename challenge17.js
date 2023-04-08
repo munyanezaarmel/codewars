9096123c7dc9224eeb00157ea90001b48351f759
unknown <munyaarmel61@gmail.com>
2023-04-08 15:55:21 +0200

    challenge 17

---
+++ challenge17.js
@@ -0,0 +1,30 @@
+/*
+. Use a constructor function to implement a 'Car'. A car has a 'make' and a 
+'speed' property. The 'speed' property is the current speed of the car in 
+km/h
+2. Implement an 'accelerate' method that will increase the car's speed by 10, 
+and log the new speed to the console
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log 
+the new speed to the console
+4. Create 2 'Car' objects and experiment with calling 'accelerate' and 
+'brake' multiple times on each of them
+Test data:
+§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 2: 'Mercedes' going at 95 km/h
+*/
+const Car = function (make, speed) {
+    this.make=make
+    this.speed=speed
+}
+Car.prototype.accelerate = function () {
+    console.log(`the acceleration of ${this.make} is ${this.speed*10}`)
+}
+Car.prototype.brake = function () {
+  console.log(`the acceleration of ${this.make} is ${this.speed / 10}`);
+};
+const bmw = new Car("BMW", 120)
+bmw.accelerate()
+bmw.brake()
+const Mercedes = new Car('Mercedes', 95)
+Mercedes.accelerate()
+Mercedes.brake()
