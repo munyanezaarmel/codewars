7b6b3075ec000cc67dddc921504e3080f516ad60
unknown <munyaarmel61@gmail.com>
2023-05-20 13:12:03 +0200

    LEETCODE

---
+++ leetcode2.js
@@ -0,0 +1,15 @@
+/*
+3. Longest Substring Without Repeating Characters
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    return s.match(//)
+};
+console.log(lengthOfLongestSubstring("abcabcbb"));
