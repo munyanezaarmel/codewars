c23c82535fa0bb7edf8a65d7511724b14ee2c6a9
unknown <munyaarmel61@gmail.com>
2023-05-24 05:21:16 +0200

    ip validation codewars

--- ipvalidation.js
+++ ipvalidation.js
@@ -4,6 +4,31 @@ IPs should be considered
 valid if they consist of four octets, with values between 0 and 255, inclusive.
 */
 function isValidIP(str) {
-  
+  const octets = str.split(".");
+
+  if (octets.length !== 4) {
+    return false;
+  }
+
+  for (let i = 0; i < octets.length; i++) {
+    const octet = octets[i];
+    if (!/^\d{1,3}$/.test(octet) || octet < 0 || octet > 255) {
+      return false;
+    }
+
+    if (octet.length > 1 && octet[0] === "0") {
+      return false;
+    }
+  }
+
+  return true;
 }
-console.log(isValidIP(" abc.0.0.0 "))
+
+// Test cases
+console.log(isValidIP("1.2.3.4")); // true
+console.log(isValidIP("123.45.67.89")); // true
+console.log(isValidIP("1.2.3")); // false
+console.log(isValidIP("1.2.3.4.5")); // false
+console.log(isValidIP("123.456.78.90")); // false
+console.log(isValidIP("123.045.067.089")); // false
+console.log(isValidIP("01.02.03.04")); // false (leading zeros)
