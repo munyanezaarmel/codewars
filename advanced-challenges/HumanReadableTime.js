1bdd519e555df7ec711ff212f8259281c9675001
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2023-10-07 12:41:57 +0200

    human readable challenge

---
+++ advanced-challenges/HumanReadableTime.js
@@ -0,0 +1,37 @@
+/*
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+You can find some examples in the test fixtures.
+
+const strictEqual = require("chai").assert.strictEqual;
+
+describe('tests', function() {
+  it('should format correctly', function() {
+    strictEqual(humanReadable(0), '00:00:00', 'humanReadable(0)');
+    strictEqual(humanReadable(59), '00:00:59', 'humanReadable(59)');
+    strictEqual(humanReadable(60), '00:01:00', 'humanReadable(60)');
+    strictEqual(humanReadable(90), '00:01:30', 'humanReadable(90)');
+    strictEqual(humanReadable(3599), '00:59:59', 'humanReadable(3599)');
+    strictEqual(humanReadable(3600), '01:00:00', 'humanReadable(3600)');
+    strictEqual(humanReadable(45296), '12:34:56', 'humanReadable(45296)');
+    strictEqual(humanReadable(86399), '23:59:59', 'humanReadable(86399)');
+    strictEqual(humanReadable(86400), '24:00:00', 'humanReadable(86400)');
+    strictEqual(humanReadable(359999), '99:59:59', 'humanReadable(359999)');
+  });
+*/
+
+
+function humanReadable (seconds) {
+    const hours =Math.trunc(seconds/3600)
+    const min = Math.trunc((seconds - (3600 * hours)) / 60);
+    const sec = Math.trunc(seconds - (3600 * hours) - (60 * min));
+    const formattedHours = hours.toString().padStart(2, '0');
+    const formattedMinutes = min.toString().padStart(2, '0');
+    const formattedSeconds = sec.toString().padStart(2, '0');
+  
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;  
+  }
+  console.log(humanReadable(60))
