f14515ab5387557369dc876da693e3c63609d9d5
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2023-12-11 11:42:20 +0200

    livewire laravel

--- laravel-gigs/resources/views/layouts/app.blade.php
+++ laravel-gigs/resources/views/layouts/app.blade.php
@@ -4,6 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        @livewireStyles
+        
+        <!-- Scripts -->
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -15,6 +20,8 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
+        @livewire('livewire-ui-modal')
+        @livewireScripts
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
--- laravel-gigs/resources/views/welcome.blade.php
+++ laravel-gigs/resources/views/welcome.blade.php
@@ -1,2 +1,2 @@
-<h1>hello app</h1>
+<li><button onclick="Livewire.dispatch('openModal', { component: 'hello-world' })"  class=" text-center bg-slate-700 px-2 py-2 text-white  rounded-md w-55">create posts</button></li>
 
