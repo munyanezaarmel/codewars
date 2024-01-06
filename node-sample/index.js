5c843f3db31a9a83e690a2f09cff23255b545807
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-01-06 12:33:16 +0200

    sample node

---
+++ node-sample/index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('File not found!');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
